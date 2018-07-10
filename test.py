8aac016f99a83ef728e1348362c248fed8035d69
Николай Пантелеев <npanteleev@proteco.local>
2018-07-10 17:26:08 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,20 @@
+a = float(input())
+b = float(input())
+c = str(input())
+if c == "*":
+    print(a * b)
+elif c == "/":
+    if b == 0:
+        print('Деление на 0!')
+    else:
+        print(a/b)
+elif c == "+":
+    print(a+b)
+elif c == "-":
+    print(a-b)
+elif c == "mod":
+    print(a%b)
+elif c == "pow":
+    print(a**b)
+elif c == "div":
+    print(a//b)
